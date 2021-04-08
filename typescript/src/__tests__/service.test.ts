67956f73545dbf3420997e29714b94e5320df07e
sylhare <sylhare@outlook.com>
2021-04-08 09:33:46 -0400

    Add test

---
+++ typescript/src/__tests__/service.test.ts
@@ -0,0 +1,18 @@
+import { graphql } from "graphql";
+import { serviceSchema } from "../schemas";
+
+describe("User Schema", () => {
+    test("Test getAllUsers query", async () => {
+        const query = `
+        {
+            user: getAllUsers {
+                name
+            }
+        }
+        `;
+        return graphql(serviceSchema, query).then((result: any) => {
+            const users = result.data.user;
+            expect(users.length).toBe(2);
+        });
+    });
+});
