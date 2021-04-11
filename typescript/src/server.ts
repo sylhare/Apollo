869035543e5f627728b2c5ad7eb9e65ab8e249e7
Sylhare <sylhare@outlook.com>
2021-04-11 19:14:30 -0400

    Fix  #1 for contentSecurityPolicy

--- typescript/src/server.ts
+++ typescript/src/server.ts
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use("*", cors());
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 app.use(compression());
 
 const server = new ApolloServer({ schema: serviceSchema });
