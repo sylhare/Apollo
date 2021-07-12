d6862d7be67ee82d14c5c3e23083219b6312cd70
sylhare <sylhare@outlook.com>
2021-07-12 11:37:06 -0400

    Update index.js

--- graphql-json/index.js
+++ graphql-json/index.js
@@ -3,9 +3,15 @@ const { readFileSync } = require("fs");
 const path = require("path");
 
 const typeDefs = gql(readFileSync(path.join(__dirname, './schema.graphql'), 'utf8'));
-const resolvers = {Query: {}};
+const resolvers = { Query: {} };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    resolverValidationOptions: {
+        requireResolversForResolveType: false,
+    },
+});
 
 server.listen().then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
