ffaca79036b78b15f3c6d182f9d144b9bc53b2ba
sylhare <sylhare@outlook.com>
2021-07-29 15:02:08 -0400

    Add ApolloExanple.test.ts

---
+++ typescript/src/__tests__/e2e/ApolloExample.test.ts
@@ -0,0 +1,14 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client/core';
+
+describe("Apollo example", () => {
+
+    // const testClient = new ApolloClient({
+    //     uri: "http://localhost:3000/graphql",
+    //     cache: new InMemoryCache({
+    //         addTypename: true
+    //     })
+    // });
+
+    test("Example call", () => {
+    })
+})
