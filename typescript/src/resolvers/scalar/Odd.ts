7614bbb854bc9bd2ad7899afc885c04a9fd17aae
sylhare <sylhare@outlook.com>
2021-11-02 10:37:27 -0400

    Add custom scalar

---
+++ typescript/src/resolvers/scalar/Odd.ts
@@ -0,0 +1,21 @@
+import { GraphQLScalarType, Kind, ValueNode } from 'graphql';
+import { UserInputError } from "apollo-server-express";
+
+function validate(value: number) {
+    if (Number.isInteger(value) && value % 2 !== 0) {
+        return value;
+    }
+    throw new UserInputError("Provided value is not an odd integer");
+}
+
+export const Odd = new GraphQLScalarType({
+    name: 'Odd',
+    description: 'Odd custom scalar type',
+    serialize: validate,
+    parseValue: validate,
+    parseLiteral(ast: ValueNode) {
+        if (ast.kind === Kind.INT) return validate(Number.parseInt(ast.value));
+        else throw new UserInputError("Provided value is not an odd integer");
+    },
+    extensions: { codegenScalarType: 'string' },
+});
