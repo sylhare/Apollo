af5cf7b0fea56fc5e14aaf4b5c818dabe3a54f6b
sylhare <sylhare@outlook.com>
2021-11-16 15:47:38 -0500

    Add dataSource index

---
+++ typescript/src/dataSource/index.ts
@@ -0,0 +1,23 @@
+import { BookDataSource } from "./books";
+
+export type AppContext = Context<AppDataSources>
+export abstract class DataSource<TContext> {}
+
+interface Context<TDataSources extends object> {
+    dataSources: TDataSources;
+    // Other elements to pass to context
+}
+
+export interface AppDataSources extends DataSources<any> {
+    books: BookDataSource<any>
+}
+
+export type DataSources<TContext> = {
+    [name: string]: DataSource<TContext>;
+};
+
+export function dataSources<TContext>(): DataSources<TContext> {
+    return {
+        books: new BookDataSource<TContext>()
+    }
+}
