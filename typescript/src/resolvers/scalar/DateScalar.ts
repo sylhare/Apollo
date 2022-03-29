330aa8ecfbe5cbbef1c74e562dd58489a4a5a34c
sylhare <sylhare@outlook.com>
2022-03-29 09:54:59 -0400

    Minimize scalar

--- typescript/src/resolvers/scalar/DateScalar.ts
+++ typescript/src/resolvers/scalar/DateScalar.ts
@@ -3,16 +3,10 @@ import { GraphQLScalarType, Kind, ValueNode } from "graphql";
 export const DateScalar = new GraphQLScalarType({
     name: 'Date',
     description: 'Date custom scalar type',
-    serialize(value: Date) {
-        return value.getTime(); // Convert outgoing Date to integer for JSON
-    },
-    parseValue(value: number) {
-        return new Date(value); // Convert incoming integer to Date
-    },
-    parseLiteral(ast: ValueNode) {
-        if (ast.kind === Kind.INT) {
-            return new Date(parseInt(ast.value, 10)); // Convert hard-coded AST string to integer and then to Date
-        }
-        return null; // Invalid hard-coded value (not an integer)
-    },
+    // Convert outgoing Date to integer for JSON
+    serialize: (value: Date) => value.getTime(),
+    // Convert incoming integer to Date
+    parseValue: (value: number) => new Date(value),
+    // Convert hard-coded AST string as integer then to Date or return Invalid hard-coded value (not an integer)
+    parseLiteral: (ast: ValueNode) => ast.kind === Kind.INT ? new Date(parseInt(ast.value, 10)) : null,
 });
