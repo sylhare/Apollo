35423b30135b3f616687865b0b4960573e653f2f
sylhare <sylhare@outlook.com>
2022-04-07 10:21:33 -0400

    Add graphql-fields-list example

--- typescript/src/resolvers/query/Movie.ts
+++ typescript/src/resolvers/query/Movie.ts
@@ -1,5 +1,6 @@
 import Movie from '../../models/movie/Movie'
 import { movies } from '../../dataSource/movies'
+import { fieldsList, fieldsMap } from 'graphql-fields-list';
 
 // Apollo expects an object as parameter
 interface MovieInput {
@@ -8,5 +9,9 @@ interface MovieInput {
 
 export async function movie(_: null, { title }: MovieInput, context:any, info: any): Promise<Movie | null> {
     console.log(`${JSON.stringify(info)}`)
+    console.log(`${JSON.stringify(fieldsList(info))}`)
+    console.log(`${JSON.stringify(fieldsMap(info, { path: 'director' }))}`)
+    console.log(`${JSON.stringify(fieldsMap(info, { skip: ['director'] }))}`)
+
     return movies.find(it => it.title === title) ?? null
 }
