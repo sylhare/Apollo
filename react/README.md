53265f4c1b3b34a016bd666de3dd00b835125638
sylhare <sylhare@outlook.com>
2022-05-04 10:40:16 -0400

    Add routes

--- react/README.md
+++ react/README.md
@@ -2,6 +2,9 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+Using:
+- react-router-dom v6
+
 ## Available Scripts
 
 In the project directory, you can run:
--- react/src/components/Root/index.tsx
+++ react/src/components/Root/index.tsx
@@ -1,10 +1,10 @@
-import App from '../App';
 import './styles.css';
+import { BrowserRouter } from "react-router-dom";
+import App from '../Nav';
 
 export default function Root() {
-  return(
-    <>
+  return (<BrowserRouter>
       <App />
-    </>
+    </BrowserRouter>
   );
 }
