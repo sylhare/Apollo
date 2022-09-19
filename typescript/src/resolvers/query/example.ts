a01aacc08d1e20a4cce73c27d0d1c268b422f554
sylhare <sylhare@outlook.com>
2022-09-19 11:21:30 -0400

    Add graphql-parse-resolve-info

--- typescript/src/resolvers/query/example.ts
+++ typescript/src/resolvers/query/example.ts
@@ -3,6 +3,7 @@ import Example from '../../models/Example'
 import { fieldsList, fieldsMap } from "graphql-fields-list";
 import { GraphQLResolveInfo } from 'graphql';
 import { AppContext } from "../../dataSource";
+import { parseResolveInfo, ResolveTree, simplifyParsedResolveInfoFragmentWithType } from 'graphql-parse-resolve-info';
 
 export async function example(parent: undefined, args: Record<string, any>, context: AppContext, info: GraphQLResolveInfo): Promise<Example> {
     console.log(`Parent in the resolver chain: ${JSON.stringify(parent)}`)
@@ -11,6 +12,9 @@ export async function example(parent: undefined, args: Record<string, any>, cont
     console.log(`Advanced GraphQL information about request: ${JSON.stringify(info)}`)
     console.log(`${JSON.stringify(fieldsList(info))}`)
     console.log(`${JSON.stringify(fieldsMap(info, { path: 'user', skip: ['__typename'] }))}`)
+    const resolvedInfo = parseResolveInfo(info) as ResolveTree;
+    console.log(`${JSON.stringify(resolvedInfo)}`)
+    console.log(`${JSON.stringify(simplifyParsedResolveInfoFragmentWithType(resolvedInfo, info.returnType))}`)
 
     return new Example('user-example-1', '1')
 }
