306c4abb2b719a35a168a9994bdc00a2985206fa
sylhare <sylhare@outlook.com>
2023-05-15 15:38:50 -0400

    Update index

--- typescript/codegen/src/index.ts
+++ typescript/codegen/src/index.ts
@@ -1,6 +1,4 @@
-import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-import { readFileSync } from 'fs';
 import {
   GraphQLAddBookMutationResponse,
   GraphQLBook,
@@ -8,46 +6,10 @@ import {
   GraphQLMutationResolvers,
   GraphQLQueryResolvers
 } from './__generated__/resolvers-types';
+import { dataSources, server } from './app/server';
 
-interface MyDataSources {
-  books: GraphQLBook[];
-}
 
-export interface MyContext {
-  token?: string;
-  dataSources?: MyDataSources
-}
 
-const resolvers = {
-  Query: {
-    books: (parent: any, args: any, context: MyContext) => context.dataSources.books,
-  } as GraphQLQueryResolvers,
-  Mutation: {
-    addBook: (_: any, { title, author }: GraphQLMutationAddBookArgs): GraphQLAddBookMutationResponse => (
-      { code: 'success', success: true, message: 'msg', book: { title, author } }
-    ),
-  } as GraphQLMutationResolvers,
-};
-
-const typeDefs = readFileSync('./schema.graphql', { encoding: 'utf-8' });
-
-const server = new ApolloServer<MyContext>({
-  typeDefs,
-  resolvers,
-});
-
-const dataSources: MyDataSources = {
-  books: [
-    {
-      title: 'The Awakening',
-      author: 'Kate Chopin',
-    },
-    {
-      title: 'City of Glass',
-      author: 'Paul Auster',
-    },
-  ]
-};
 
 const { url } = await startStandaloneServer(server, {
   context: async ({ req }) => ({
