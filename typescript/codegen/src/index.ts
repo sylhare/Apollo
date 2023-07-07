dfc49986b15cddbccde9785fdfa7272ccb5beef4
sylhare <sylhare@outlook.com>
2023-07-07 09:52:26 -0400

    Clean up index

--- typescript/codegen/src/index.ts
+++ typescript/codegen/src/index.ts
@@ -1,22 +1,10 @@
 import { startStandaloneServer } from '@apollo/server/standalone';
-import {
-  GraphQLAddBookMutationResponse,
-  GraphQLBook,
-  GraphQLMutationAddBookArgs,
-  GraphQLMutationResolvers,
-  GraphQLQueryResolvers
-} from './__generated__/resolvers-types';
 import { dataSources, server } from './app/server';
 
-
-
-
-const { url } = await startStandaloneServer(server, {
+startStandaloneServer(server, {
   context: async ({ req }) => ({
     token: req.headers.token,
     dataSources,
   }),
   listen: { port: 4000 },
-});
-
-console.log(`🚀  Server ready at: ${url}`);
+}).then(({ url }) => console.log(`🚀  Server ready at: ${url}`));
